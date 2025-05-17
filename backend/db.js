b223475636014ffb5c635e237b9a665955a999f4
Abdul Hanan Nawaz <hanannawaz0@gmail.com>
2025-05-17 23:42:09 +0200

    ssl error resolved

--- backend/db.js
+++ backend/db.js
@@ -4,11 +4,13 @@ const mongoose = require ('mongoose');
 main().catch(err => console.log(err));
 async function connectToMongo()
 {
-await mongoose.connect(process.env.MONGO_URI, {
+  await mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   ssl: true
 });
+
+}
 async function main() {
   connectToMongo();
 }
